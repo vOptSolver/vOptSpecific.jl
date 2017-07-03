a1b9003422e7f8da23069f7be7dc4b73dfd5305d
Gauthier Soleilhac <gsoleilhac@gmail.com>
2017-07-03 16:50:35 +0200

    Adds a MO Scheduling Problem Solver

--- src/vOptSpecific.jl
+++ src/vOptSpecific.jl
@@ -1,12 +1,15 @@
 module vOptSpecific
 
-export load2LAP, set2LAP, vSolve, LAP_Przybylski2008 
+export vSolve,
+    load2LAP, set2LAP, LAP_Przybylski2008,
+    load2OSP, set2OSP, OSP_VanWassenhove1980, generateHardInstance
         #KP, solveKP, KP_Jorge2010
 
 const LIBPATH = joinpath(dirname(@__FILE__),"..","deps")
 
 
 include("LAP.jl")
+include("jMOScheduling.jl")
 # include("KP.jl")
 
 
