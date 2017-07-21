804bfd29e381fa636940dc0cf46116f20236351d
Gauthier Soleilhac <gsoleilhac@gmail.com>
2017-07-21 20:20:55 +0200

    updates OSP example

--- examples/set2OSP.jl
+++ examples/set2OSP.jl
@@ -1,10 +1,13 @@
 n =4 
 p = [2, 4, 3, 1]
 d = [1, 2, 4, 6]
-r = [0, 0, 0, 0]
-w = [1, 1, 1, 1]
+
 
 using vOptSpecific
 
-id = set2OSP(n, p, d, r, w)
-z1, z2 , S = vSolve(id)
+id = set2OSP(n, p, d)
+z1, z2 , S = vSolve(id) #Default solver is OSP_Van_Wassenhove1980()
+
+for i = 1:length(S)
+    println("(", z1[i], ", ", z2[i], ") : ", S[i])
+end
