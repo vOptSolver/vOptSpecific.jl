1628ce227989dfd0d78da4c011b325dd7e222926
vOpt <vopt@univ-nantes.fr>
2021-07-25 09:37:47 +0200

    code polished

--- examples/set2LAPfull.jl
+++ examples/set2LAPfull.jl
@@ -8,9 +8,6 @@ myInstance = set2LAP( n , C1 , C2 )
 algorithm = LAP_Przybylski2008( )
 z1, z2, S = vSolve( myInstance , algorithm )
 
-id = set2LAP(n, C1, C2)
-z1,z2,solutions = vSolve(id)
-
 for i = 1:length(z1)
-    println("(" , z1[i], " | ", z2[i], ") : ", solutions[i,:])
+    println("(" , z1[i], " | ", z2[i], ") : ", S[i,:])
 end
